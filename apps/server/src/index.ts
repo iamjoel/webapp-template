8c65764d679d34c270a0213412a05004ce92f167
Joel <iamjoel007@gmail.com>
2026-07-13 22:08:26 +0800

    feat: init

---
+++ apps/server/src/index.ts
@@ -0,0 +1,48 @@
+import { createContext } from "@app/api/context";
+import { appRouter } from "@app/api/routers/index";
+import { env } from "@app/env/server";
+import { trpcServer } from "@hono/trpc-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use(logger());
+app.use(
+  "/*",
+  cors({
+    origin: env.CORS_ORIGIN,
+    allowMethods: ["GET", "POST", "OPTIONS"],
+  }),
+);
+
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+    createContext: (_opts, context) => {
+      return createContext({ context });
+    },
+  }),
+);
+
+app.get("/", (c) => {
+  return c.text("OK");
+});
+
+import { serve } from "@hono/node-server";
+
+export default app;
+
+if (!process.env.VERCEL) {
+  serve(
+    {
+      fetch: app.fetch,
+      port: 3000,
+    },
+    (info) => {
+      console.log(`Server is running on http://localhost:${info.port}`);
+    },
+  );
+}
